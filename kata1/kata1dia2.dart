babdc1ce21fe6ebe672985b18e503dd8b216774d
samucorea <samuel.alejandro100@hotmail.com>
2021-05-13 10:35:48 -0400

    Testing funcion getNumbersOfArrayHigherThanMeanArray

--- kata1/kata1dia2.dart
+++ kata1/kata1dia2.dart
@@ -1,42 +1,38 @@
-
-num getMean(List<num> arr)
-{
+num getMean(List<num> arr) {
   num acc = 0;
   num mean;
 
   arr.forEach((element) {
     acc += element;
-  })
+  });
 
   mean = acc / arr.length;
 
   return mean;
-
 }
 
-
-List<num> getNumbersOfArrayHigherThanMeanArray(List<num> arr)
-{
+List<num> getNumbersOfArrayHigherThanMeanArray(List<num> arr) {
   num mean = getMean(arr);
+
   int indice = 0;
 
   arr.sort();
 
-  for(int i = 0; i < arr.length; i++)
-  {
-    if(arr[i] > mean)
-    {
+  for (int i = 0; i < arr.length; i++) {
+    if (arr[i] > mean) {
       indice = i;
+      break;
     }
   }
 
-  List<num> numbersOfArrayHigherThanMeanArray = arr.getRange(indice, arr.length).toList();
+  List<num> numbersOfArrayHigherThanMeanArray =
+      arr.getRange(indice, arr.length).toList();
 
   return numbersOfArrayHigherThanMeanArray;
-
-
 }
-int main()
-{
 
+void main() {
+  List<num> arreglo = [6, 3, 4, 5, 7];
+
+  print(getNumbersOfArrayHigherThanMeanArray(arreglo));
 }
