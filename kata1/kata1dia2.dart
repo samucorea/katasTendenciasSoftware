14abc782c9dda0a12d8a72e070e624816b2ac10d
samucorea <samuel.alejandro100@hotmail.com>
2021-05-13 10:26:16 -0400

    Creada funcion getMean

--- kata1/kata1dia2.dart
+++ kata1/kata1dia2.dart
@@ -1,1 +1,19 @@
 
+num getMean(List<num> arr)
+{
+  num acc = 0;
+  num mean;
+
+  arr.forEach((element) {
+    acc += element;
+  })
+
+  mean = acc / arr.length;
+
+  return mean;
+
+}
+int main()
+{
+
+}
