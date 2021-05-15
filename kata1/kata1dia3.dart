80fb5b6f15144539493845f90a1bfa85a6ffb513
samucorea <samuel.alejandro100@hotmail.com>
2021-05-14 21:33:09 -0400

    getNumbersHigherThanMean done and working

--- kata1/kata1dia3.dart
+++ kata1/kata1dia3.dart
@@ -1,4 +1,4 @@
-getMean(List<num> arr) {
+num getMean(List<num> arr) {
   num mean = 0;
   num sum = arr.reduce((a, b) => a + b);
 
@@ -7,4 +7,28 @@ getMean(List<num> arr) {
   return mean;
 }
 
-void main() {}
+List<num> getNumbersHigherThanMean(List<num> arr) {
+  List<num> numbersHigherThanMean;
+  num mean = getMean(arr);
+  int indexFirstHigherNum = 0;
+
+  arr.sort();
+
+  for (int i = 0; i < arr.length; i++) {
+    if (arr[i] > mean) {
+      indexFirstHigherNum = i;
+      break;
+    }
+  }
+
+  numbersHigherThanMean =
+      arr.getRange(indexFirstHigherNum, arr.length).toList();
+
+  return numbersHigherThanMean;
+}
+
+void main() {
+  List<num> numbers = [3, 4, 5, 6, 7, 8];
+
+  print(getNumbersHigherThanMean(numbers));
+}
